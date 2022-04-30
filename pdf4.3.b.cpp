ff513c09b6649d11c05170db58db5cbbba9bb3b6
Omkar <omkarpatil3462@gmail.com>
2022-04-30 13:30:16 +0530

    Triange pattern

---
+++ pdf4.3.b.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+void pattern(int row, int col)
+{
+
+    for (int i = 1; i <= row; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "*\t";
+        }
+        cout << "\n";
+    }
+}
+
+int main()
+{
+    int iRow, iCol;
+    cout << "Please enter rows and columns\n";
+    cin >> iRow >> iCol;
+
+    pattern(iRow, iCol);
+
+    return 0;
+}
