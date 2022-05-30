7d63be98054ed3e2ed73419436c69b22f38446c9
omkarpatilkit <41175678+omkarpatilkit@users.noreply.github.com>
2022-05-30 12:27:25 +0000

    precise sq

--- sqRoot.cpp
+++ sqRoot.cpp
@@ -27,10 +27,27 @@ int sqRoot(int a)
     return ans;
 }
 
+double prciseSqRoot(int a)
+{
+    int temp = sqRoot(a);
+    double ans = 0;
+
+    for (double x = 0.1; (x + (double)temp) * (x + (double)temp) <= a; x = x + 0.1)
+    {
+        ans = x + (double)temp;
+    }
+    temp = ans;
+    for (double x = 0.01; (x + (double)temp) * (x + (double)temp) <= a; x = x + 0.01)
+    {
+        ans = x + (double)temp;
+    }
+    return ans;
+}
+
 int main()
 {
-    int a = 50;
-    int res = sqRoot(a);
+    int a = 30;
+    double res = prciseSqRoot(a);
     cout << res << endl;
     return 0;
 }
